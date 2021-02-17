d8ad35f21d952ed3d95258cabe60f25c0df89680
Pai Peng <p.peng@s2icode.com>
2021-02-17 21:03:44 +0800

    add component event

--- components/my-component-name/my-component-name.vue
+++ components/my-component-name/my-component-name.vue
@@ -1,6 +1,7 @@
 <template>
 	<view>
 		{{myComponentName}} {{title.name}} {{title.index}}
+		<view type="default" @click.stop="open" style="height: 30px;">点击</view>
 	</view>
 </template>
 
@@ -10,6 +11,11 @@
 		data() {
 			return {
 				"myComponentName":"Pai Peng"
+			}
+		},
+		methods:{
+			open(){
+				console.log("open");
 			}
 		}
 	}
