df5d825f6a9bd8fdbefeb4d18c8c12433dddd9fe
Pai Peng <p.peng@s2icode.com>
2021-02-17 17:56:40 +0800

    v-model

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,24 +1,9 @@
 <template>
 	<view class="content">
 		<view>
-			<button v-on:click="counter += 1">Add 1</button>
-			<text>The button above has been clicked {{ counter }} times.</text>
+			<input v-model="message" placeholder="edit me">
+			<text>Message is: {{ message }}</text>
 		</view>
-		<view>
-			<!-- `greet` 是在下面定义的方法名 -->
-			<button v-on:click="greet">Greet</button>
-		</view>
-		<view>
-			<button v-on:click="say('hi')">Say hi</button>
-			<button v-on:click="say('what')">Say what</button>
-		</view>
-		
-		<view>
-			<button v-on:click="warn('Form cannot be submitted yet.', $event)">
-			  Submit
-			</button>
-		</view>
-
 	</view>
 </template>
 
@@ -27,32 +12,7 @@
 	export default {
 		data() {
 			return {
-				counter:0,
-				name: 'Vue.js'
-			}
-		},
-		// 在 `methods` 对象中定义方法
-		methods: {
-			greet(event){
-				// `event` 是原生 DOM 事件
-				console.log(event);
-				uni.showToast({
-					title: 'Hello ' + this.name + '!'
-				});
-			},
-			say(message) {
-				uni.showToast({
-					title: message
-				});
-			},
-			warn(message, event) {
-				// 现在我们可以访问原生事件对象
-				if (event) {
-					event.preventDefault()
-				}
-				uni.showToast({
-					title: message
-				});
+				message:""
 			}
 		},
 		onLoad() {
